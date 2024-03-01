ab06a577669ed224b6d305b2dfcec7443d75d20c
pqtatos <mrhotdog@yandex.ru>
2024-03-01 17:28:09 +0500

    new branch

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-#include "Services/network.hpp"
-#include "Services/bot.hpp"
-
 int main()
 {
-  Bot bot("6329343331:AAER5UjGOmLh_G7wBbEG7VNtBvyYQBD84Z0", ParseMode::HTML);
-  Network net;
-  net.ConnectBot(&bot);
-  net.StartPolling();
   return 0;
 }
